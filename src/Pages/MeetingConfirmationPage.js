59cb498f59cd18cb9859f0d5d6797e7ba1f7e950
ramgpal <ramgpal912@gmail.com>
2024-07-09 00:06:06 +0530

    changes made in meetingConfiemation page

--- src/Pages/MeetingConfirmationPage.js
+++ src/Pages/MeetingConfirmationPage.js
@@ -12,6 +12,12 @@ const MeetingConfirmationPage = () => {
         <p className="text-lg">You've scheduled a meeting on</p>
         <p className="text-xl font-semibold">{slot.date}</p>
         <p className="text-xl font-semibold">{slot.time}</p>
+        <p className="text-lg mt-4">Meeting URL:</p>
+        <p className="text-xl font-semibold">
+          <a href="https://meet.google.com/chy-kicz-iwo" target="_blank" rel="noopener noreferrer">
+            https://meet.google.com/chy-kicz-iwo
+          </a>
+        </p>
       </div>
     </div>
   );
